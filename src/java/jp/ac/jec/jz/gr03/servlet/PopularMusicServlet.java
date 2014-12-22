423bbd28fe80277636aa516d4da17fe711bcfca6
12jz0115 <ydt.hran2@gmail.com>
2014-12-22 10:39:47 +0900

    注目動画表示機能を追加

---
+++ src/java/jp/ac/jec/jz/gr03/servlet/PopularMusicServlet.java
@@ -0,0 +1,90 @@
+package jp.ac.jec.jz.gr03.servlet;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import jp.ac.jec.jz.gr03.dao.AdvertisementDAO;
+import jp.ac.jec.jz.gr03.dao.entityresultset.AdvertisementResultSet;
+
+/**
+ *
+ * @author yada
+ */
+public class PopularMusicServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        
+        AdvertisementResultSet ads = selectPopulars();
+        
+        request.setAttribute("advertisements", ads);
+        request.getRequestDispatcher("popularMusic.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    private AdvertisementResultSet selectPopulars() {
+        AdvertisementDAO dao = new AdvertisementDAO();
+        AdvertisementResultSet ads = null;
+        try {
+            ads = dao.selectPopularMusics(30, 0);
+        } catch (IOException ex) {
+            Logger.getLogger(PopularMusicServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ads;
+    }
+}
