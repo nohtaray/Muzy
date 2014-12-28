43f61221632048c652dfbafc2743c1d506a045a2
12jz0115 <ydt.hran2@gmail.com>
2014-12-28 17:15:12 +0900

    EntityResultSet を Iterator ではなくした
    iterator() 内で return this していると複数回イテレートできない
    Iterable であれば Iterator である必要性もない
    readRow() は互換性のために残す

--- src/java/jp/ac/jec/jz/gr03/dao/entityresultset/EntityResultSet.java
+++ src/java/jp/ac/jec/jz/gr03/dao/entityresultset/EntityResultSet.java
@@ -8,95 +8,98 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /**
  *
  * @author yada
  * @param <T>
  */
-abstract public class EntityResultSet<T> implements Iterator<T>, Iterable<T> {
-    
+abstract public class EntityResultSet<T> implements Iterable<T> {
+
     private final ResultSet rs;
-    
+    private Iterator<T> it;
+
     public EntityResultSet(ResultSet rs) {
         this.rs = rs;
+        this.it = null;
     }
-    
+
     /**
-     * ResultSet を1行進め、その行をエンティティとして返す。
-     * 取り出せない場合は null を返す。
+     * ResultSet を1行進め、その行をエンティティとして返す。 取り出せない場合は null を返す。
+     *
      * @return
-     * @throws SQLException 
      */
-    public T readRow() throws SQLException {
-        // resultSet.getXXX(colName) すると、colName が存在しない場合例外が発生するので
-        // まず存在する列名をすべて Map に入れて、そこから取得を試みる。
-        Map<String, Object> row = readMapRow(rs);
-        if (row != null) {
-            return pickEntityFrom(row);
-        } else {
-            return null;
+    public T readRow() {
+        if (it == null) {
+            it = iterator();
         }
+        return it.next();
     }
-    
+
     /**
-     * MapからEntityを取り出す。MapのキーはDBのテーブル名。
-     * 型参考: http://dounanda.s140.xrea.com/mapping.html
+     * MapからEntityを取り出す。MapのキーはDBのテーブル名。 型参考:
+     * http://dounanda.s140.xrea.com/mapping.html
+     *
      * @param row
-     * @return 
+     * @return
      */
     abstract protected T pickEntityFrom(Map<String, Object> row);
     
-    /**
-     * ResultSet を1行進め、その行を Map として返す。
-     * 取り出せない場合は null を返す。
-     * @param rs
-     * @return
-     * @throws SQLException 
-     */
-    private Map<String, Object> readMapRow(ResultSet rs)
-            throws SQLException {
-        boolean rowExists = rs.next();
-        if (!rowExists) return null;
-        
-        Map<String, Object> row = new HashMap<>();
-        ResultSetMetaData meta = rs.getMetaData();
-        
-        for (int i = 1; i <= meta.getColumnCount(); i++) {
-            row.put(meta.getColumnName(i), rs.getObject(i));
-        }
-        
-        return row;
-    }
-
-    @Override
-    public boolean hasNext() {
-        try {
-            boolean has = rs.next();
-            rs.previous(); // カーソルが進んじゃうので戻す
-            return has;
-        } catch (SQLException ex) {
-            Logger.getLogger(EntityResultSet.class.getName()).log(Level.SEVERE, null, ex);
-            return false;
-        }
-    }
-
-    @Override
-    public T next() {
-        try {
-            return readRow();
-        } catch (SQLException ex) {
-            Logger.getLogger(EntityResultSet.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
-        }
-    }
-
-    @Override
-    public void remove() {
-        throw new UnsupportedOperationException("Not supported."); //To change body of generated methods, choose Tools | Templates.
-    }
-
     @Override
     public Iterator<T> iterator() {
-        return this;
+        return new EntityResultSetIterator(rs);
+    }
+
+    private class EntityResultSetIterator implements Iterator<T> {
+
+        private final ResultSet rs;
+
+        public EntityResultSetIterator(ResultSet rs) {
+            try {
+                rs.beforeFirst();
+            } catch (SQLException ex) {
+                Logger.getLogger(EntityResultSet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            this.rs = rs;
+        }
+
+        @Override
+        public boolean hasNext() {
+            try {
+                boolean has = rs.next();
+                rs.previous(); // カーソルが進んじゃうので戻す
+                return has;
+            } catch (SQLException ex) {
+                Logger.getLogger(EntityResultSetIterator.class.getName()).log(Level.SEVERE, null, ex);
+                return false;
+            }
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                return null;
+            }
+            
+            // resultSet.getXXX(colName) すると、colName が存在しない場合例外が発生するので
+            // まず存在する列名をすべて Map に入れて、そこから取得を試みる。
+            Map<String, Object> row = new HashMap<>();
+            try {
+                rs.next();
+                ResultSetMetaData meta = rs.getMetaData();
+                for (int i = 1; i <= meta.getColumnCount(); i++) {
+                    row.put(meta.getColumnName(i), rs.getObject(i));
+                }
+            } catch (SQLException ex) {
+                // 例外の握り潰し（どうするのがいいんだろう）
+                Logger.getLogger(EntityResultSet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return pickEntityFrom(row);
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Not supported.");
+        }
     }
 }
