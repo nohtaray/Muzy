d413e3c3e47e567ee77493451b77d92b23f91743
12jz0115 <ydt.hran2@gmail.com>
2014-12-31 15:33:31 +0900

    楽曲削除機能を追加

---
+++ src/java/jp/ac/jec/jz/gr03/servlet/DeleteMusicServlet.java
@@ -0,0 +1,124 @@
+package jp.ac.jec.jz.gr03.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import jp.ac.jec.jz.gr03.dao.MusicDAO;
+import jp.ac.jec.jz.gr03.entity.Music;
+import jp.ac.jec.jz.gr03.util.Authorizer;
+
+/**
+ *
+ * @author yada
+ */
+public class DeleteMusicServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        HttpSession session = request.getSession();
+        Authorizer auth = new Authorizer(session);
+        
+        String idStr = request.getParameter("id");
+        if (idStr == null) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "パラメータが足りません");
+            return;
+        }
+        
+        int id;
+        try {
+            id = Integer.parseInt(idStr);
+        } catch (NumberFormatException e) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "ID は数値で指定してください");
+            return;
+        }
+        
+        Music music = selectMusic(id);
+        if (music == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "楽曲が存在しません");
+            return;
+        } else if (!auth.hasLoggedInAs(music.artist.user)) {
+            // 自分の楽曲じゃない
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "許可がありません");
+            return;
+        }
+        
+        delete(music);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    
+    private Music selectMusic(int id) {
+        MusicDAO dao = new MusicDAO();
+        try {
+            return dao.selectById(id);
+        } catch (IOException ex) {
+            Logger.getLogger(DeleteMusicServlet.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    private void delete(Music music) {
+        MusicDAO dao = new MusicDAO();
+        try {
+            dao.delete(music);
+        } catch (IOException ex) {
+            Logger.getLogger(DeleteMusicServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
