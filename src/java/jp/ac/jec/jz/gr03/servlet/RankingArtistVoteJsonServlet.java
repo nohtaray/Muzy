c7396a567f0c78a4ec845f65902b7a5f5813f17b
12jz0115 <ydt.hran2@gmail.com>
2015-01-20 12:24:14 +0900

    アーティストの投票数ランキングを JSON で出力するサーブレットを追加

---
+++ src/java/jp/ac/jec/jz/gr03/servlet/RankingArtistVoteJsonServlet.java
@@ -0,0 +1,105 @@
+package jp.ac.jec.jz.gr03.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import jp.ac.jec.jz.gr03.dao.ArtistVoteDAO;
+import jp.ac.jec.jz.gr03.dao.entityresultset.ArtistVoteResultSet;
+import jp.ac.jec.jz.gr03.entity.ArtistMylistDetail;
+import jp.ac.jec.jz.gr03.entity.ArtistVote;
+import net.arnx.jsonic.JSON;
+
+/**
+ *
+ * @author yada
+ */
+public class RankingArtistVoteJsonServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        ArtistVoteResultSet artistVotes = selectArtistVotes();
+        
+        // リンクしてるユーザ情報などを出力しないようフィルタリングする
+        List<Map<String, Object>> outs = new ArrayList<>();
+        for (ArtistVote artistVote : artistVotes) {
+            Map<String, Object> row = new HashMap<>();
+            Map<String, Object> artist = new HashMap<>();
+            artist.put("artist_id", artistVote.artist.artistId);
+            artist.put("user_id", artistVote.artist.user.userId);
+            artist.put("name", artistVote.artist.name);
+            artist.put("introduction", artistVote.artist.introduction);
+            artist.put("header_image_file", artistVote.artist.headerImageFile);
+            artist.put("created_at", artistVote.artist.createdAt);
+            artist.put("updated_at", artistVote.artist.updatedAt);
+            row.put("artist", artist);
+            row.put("vote_count", artistVote.voteCount);
+            row.put("spent_tickets_sum", artistVote.spentTicketsSum);
+            outs.add(row);
+        }
+        response.getWriter().print(JSON.encode(outs));
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    private ArtistVoteResultSet selectArtistVotes() throws IOException {
+        ArtistVoteDAO dao = new ArtistVoteDAO();
+        return dao.select(30, 0);
+    }
+}
