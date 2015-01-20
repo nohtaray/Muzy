5b8dc522cd43e39c484bddad32dcb1b83ec5896b
12jz0115 <ydt.hran2@gmail.com>
2015-01-20 15:15:06 +0900

    トップページにアーティストと楽曲のランキングを表示するように変更

--- web/index.jsp
+++ web/index.jsp
@@ -3,6 +3,7 @@
 <c:import url="/layout/application.jsp">
     <c:param name="title" value="ページのタイトル" />
     <c:param name="header">
+        <script type="text/javascript" src="js/index.js"></script>
     </c:param>
     <c:param name="content">
 
@@ -10,5 +11,22 @@
             こんにちは！Muzyです！
         </p>
 
+        <div id="rankings">
+            アーティストランキング
+            <div id="ranking-artists">
+                <div id="ranking-artist-template" class="ranking-artist hidden">
+                    <div class="ranking-artist-name"></div>
+                    <div class="ranking-artist-introduction"></div>
+                </div>
+            </div>
+            楽曲ランキング
+            <div id="ranking-musics">
+                <div id="ranking-music-template" class="ranking-music hidden">
+                    <div class="ranking-music-title"></div>
+                    <div class="ranking-music-description"></div>
+                </div>
+            </div>
+        </div>
+        
 </c:param>
 </c:import>
---
+++ web/js/index.js
@@ -0,0 +1,37 @@
+/*global $ */
+$(function() {
+    loadRankingJson('RankingArtistVoteJsonServlet', function(artistVotes) {
+        var $container = $('#ranking-artists');
+        var $template = $('#ranking-artist-template');
+        artistVotes.forEach(function(artistVote) {
+            var $artist = $template.clone();
+            var $name = $('<a>', {
+                href: 'ArtistServlet?id=' + artistVote['artist']['artist_id']
+            }).text(artistVote['artist']['name']);
+            $artist.find('.ranking-artist-name').append($name);
+            $artist.find('.ranking-artist-introduction').text(artistVote['artist']['introduction']);
+            $artist.removeAttr('id').removeClass('hidden').appendTo($container);
+        });
+    });
+    loadRankingJson('RankingMusicAdvertisementJsonServlet', function(musicAdvertisements) {
+        var $container = $('#ranking-musics');
+        var $template = $('#ranking-music-template');
+        musicAdvertisements.forEach(function(musicAdvertisement) {
+            var $music = $template.clone();
+            var $title = $('<a>', {
+                href: 'MusicServlet?id=' + musicAdvertisement['music']['music_id']
+            }).text(musicAdvertisement['music']['title']);
+            $music.find('.ranking-music-title').append($title);
+            $music.find('.ranking-music-description').text(musicAdvertisement['music']['description']);
+            $music.removeAttr('id').removeClass('hidden').appendTo($container);
+        });
+    });
+
+    function loadRankingJson(url, onSuccess) {
+        $.ajax({
+            type: 'GET',
+            url: url,
+            dataType: 'json'
+        }).success(onSuccess);
+    }
+});
